18c00b26491500eb8a5c1d0571bfa483f85523f6
admin <deepakapl05@gmail.com>
2026-04-27 12:15:35 +0530

    Fix router for GitHub Pages subpath

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     }, []);
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/Jewellery/">
             <div className="app">
                 <Navbar />
                 <Routes>
